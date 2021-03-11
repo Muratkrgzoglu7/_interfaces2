b4a1e0cfec105e92caaf6a174ff686d63b3eb1af
Murat Karagöz <mrt.kara.1767@gmail.com>
2021-03-11 22:37:12 +0300

    meyin

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace İnterFacesDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IWork[] works = new IWork[3] {new Worker(),new Manager(),new Robot() };
+			foreach (var worker in works)
+			{
+				worker.Work();
+			}
+			IEat[] eats = new IEat[2] {new Worker(),new Manager()};
+			foreach (var eat in eats)
+			{
+				eat.Eat();
+			}
+		}
+		interface IWork
+		{
+			
+			void Work();
+
+		}
+		interface IGetSallary
+		{
+				void GetSallary();
+	}
+		interface IEat
+		{
+			void Eat();
+		}
+		class Manager : IWork, IEat, IGetSallary
+		{
+			public void Eat()
+			{
+				throw new NotImplementedException();
+			}
+
+			public void GetSallary()
+			{
+				throw new NotImplementedException();
+			}
+
+			public void Work()
+			{
+				throw new NotImplementedException();
+			}
+		}
+		class Worker : IWork, IGetSallary, IEat
+		{
+			public void Eat()
+			{
+				throw new NotImplementedException();
+			}
+
+			public void GetSallary()
+			{
+				throw new NotImplementedException();
+			}
+
+			public void Work()
+			{
+				throw new NotImplementedException();
+			}
+		}
+		class Robot : IWork
+		{
+			public void Work()
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
